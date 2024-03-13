f78c0717c5970b5f9992832818681c116c1ffcb1
paulodelosrey <135145158+paulodelosrey@users.noreply.github.com>
2024-03-13 17:29:10 -0500

    terminada la logica

--- main.js
+++ main.js
@@ -4,16 +4,9 @@ const gif = document.getElementById("gif")
 const encriptar = document.getElementById("encriptar")
 const desencriptar = document.getElementById("desencriptar")
 const reiniciar = document.getElementById("reiniciar")
-
-
-const matrizDeEncriptacion = [
-    ["e", "enter"],
-    ["i", "imes"],
-    ["a", "ai"],
-    ["o", "ober"],
-    ["u", "ufat"],
-];
-
+const parrafoTextoEncriptado = document.getElementById("parrafoTextoEncriptado")
+const textoEncriptado = document.getElementById("textoEncriptado")
+const copiar = document.getElementById("copiar")
 
 textoAEncriptar.addEventListener("focus", () => {
     parrafo.textContent = "Escribiendo..."
@@ -31,11 +24,61 @@ textoAEncriptar.addEventListener("input", (e) => {
     textoAEncriptar.value = textoMinusculas.replace(/[^a-zA-Z0-9 ]/g, "")
     });
 
-encriptar.addEventListener("click", ()=> {
-    for(let i = 0; i < matrizDeEncriptacion; i++) {
-        if (textoAEncriptar.value.includes(matrizDeEncriptacion[i[0]])) {
-            textoAEncriptar.value.replace(matrizDeEncriptacion[i[0]], matrizDeEncriptacion[i[1]])
-        }
-    }     
-})
+    
 
+    encriptar.addEventListener("click", () => {
+        let texto = textoAEncriptar.value.toLowerCase()
+        let matrizDeEncriptacion = [
+        ["e", "enter"],
+        ["i", "imes"],
+        ["a", "ai"],
+        ["o", "ober"],
+        ["u", "ufat"],
+        ];
+        for (let i = 0; i < matrizDeEncriptacion.length; i++) {
+        if (texto.includes(matrizDeEncriptacion[i][0])) {
+            texto = texto.replaceAll(matrizDeEncriptacion[i][0], matrizDeEncriptacion[i][1]);
+        }
+        }
+        parrafo.style.display = "none"
+        gif.style.display = "none"
+        parrafoTextoEncriptado.style.display = "block"
+        textoEncriptado.textContent = texto
+        textoEncriptado.style.display = "block"
+        desencriptar.disabled= false
+        copiar.disabled = false
+        reiniciar.disabled = false
+    });
+
+
+    reiniciar.addEventListener("click", ()=>{
+        location.reload()
+    })
+    
+    copiar.addEventListener("click", () => {
+        textoEncriptado.select()
+        document.execCommand("copy")
+        alert("Copiado al portapapeles")
+    })
+
+    desencriptar.addEventListener("click", () => {
+        
+        let texto = textoAEncriptar.value.toLowerCase()
+        let matrizDeEncriptacion = [
+        ["e", "enter"],
+        ["i", "imes"],
+        ["a", "ai"],
+        ["o", "ober"],
+        ["u", "ufat"],
+        ];
+        for (let i = 0; i < matrizDeEncriptacion.length; i++) {
+        if (texto.includes(matrizDeEncriptacion[i][1])) {
+            texto = texto.replaceAll(matrizDeEncriptacion[i][1], matrizDeEncriptacion[i][0]);
+        }
+        }
+        parrafo.style.display = "none"
+        gif.style.display = "none"
+        parrafoTextoEncriptado.style.display = "block"
+        textoEncriptado.textContent = texto
+        textoEncriptado.style.display = "block"
+    })
